e659749ce661c08fb5947f5fe3aa980b6edab07d
ThyagoHSR <thyagohsr@gmail.com>
2023-05-13 19:24:38 -0300

    Modificações

--- main.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("btn-scroll-top").style.display = "block";
-  } else {
-    document.getElementById("btn-scroll-top").style.display = "none";
-  }
-}
